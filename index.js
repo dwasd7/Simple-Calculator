68e2e5ad8d12d45b13cc9c268b96a340f3e61843
Diwas <diwasdulal2002@gmail.com>
2022-02-21 21:57:23 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+let display = document.getElementById('display');
+
+let buttons = Array.from(document.getElementsByClassName('button'));
+console.log(buttons);
+
+buttons.forEach( button => {
+  button.addEventListener('click', (e) => {
+      if (e.target.innerText == 'C'){
+        display.innerText = '';
+      }
+      else if(e.target.innerText == '←'){
+        display.innerText = display.innerText.slice(0,-1);
+      }
+      else if(e.target.innerText == '=')
+      { 
+        try{
+            display.innerText = eval(display.innerText);
+        }
+        catch(err){
+          display.innerText = "Error! STOOPID!!";
+        }
+      }
+      else
+        display.innerText += e.target.innerText;
+  });
+});
